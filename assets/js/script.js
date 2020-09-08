163bc9fcdb5268e7823cc0a2ffb585b8533e7491
Rangel Pereira <dev@rangelpereira.com.br>
2020-09-08 05:58:56 -0300

    Limitando movimentação

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,14 +57,19 @@ function start() { // Inicio da funçao start()
 	
             if (jogo.pressionou[TECLA.W]) {
                 var topo = parseInt($("#jogador").css("top"));
-                $("#jogador").css("top",topo-10);
-            
+                    if (topo >= 20) {
+                        $("#jogador").css("top",topo-10);
+                    }          
             }
             
             if (jogo.pressionou[TECLA.S]) {
                 
                 var topo = parseInt($("#jogador").css("top"));
-                $("#jogador").css("top",topo+10);	
+                
+                    if (topo <= 430) {	
+                        $("#jogador").css("top",topo+10);
+                            
+                    }
             }
             
             if (jogo.pressionou[TECLA.D]) {
